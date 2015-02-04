de76825bf4d57c6bbc5c35a35ef2b3f5c73ec5f9
Angie Green <angie@greenorleans.com>
2015-02-04 12:37:15 -0600

    Add week two Ruby test

---
+++ week_2_homework/Ruby_test.rb
@@ -0,0 +1,92 @@
+# # TEST ONE
+# input = gets.chomp
+# puts "Hello, #{input}. Nice to meet you."
+
+# # TEST TWO
+# def sum_numbers(x, y, z)
+#   puts x + y + z
+# end
+
+# sum_numbers(1, 1, 1)
+
+# #TEST THREE
+# to_do = ["wash the car", "buy groceries", "finish homework", "pay the bills"]
+
+# to_do.each do |x|
+#   puts "Don't forget to #{x}."
+# end
+
+# # TEST FOUR
+# 16
+
+# # TEST FIVE
+# "Sarah"
+
+# # TEST SIX
+# wild_cats.push('bobcat')
+
+# # TEST SEVEN
+# user1 = {:firstname => "Johnny", :birthplace => "Seattle"}
+# puts user1[:birthplace]
+
+# # TEST EIGHT
+# user1["current city"] = "Atlanta, GA"
+
+# # TEST NINE
+# alpha_soup[2]
+
+# # TEST TEN
+# alphabits["k"]
+
+# TEST ELEVEN   !!!!!!!!!!!!!!!!
+# def number(x)
+#   puts x until x == 7
+#   num
+# end
+
+# num = number(rand(1..10))
+
+# x = 0
+# until x == 7
+#   x = rand(1..10)
+#   puts x
+# end
+
+# def number(x)
+#   until x == 7
+#     x = rand(1..10)
+#     puts x
+#   end
+# end
+
+# num = number(0)
+
+# TEST TWELVE
+# def number(x)
+#   array = []
+#   until x == 7
+#     x = rand(1..10)
+#     array.push(x) if x < 7
+#   end
+#   puts array
+#   puts "There are " + array.count.to_s + " numbers under 6."
+# end
+
+# num = number(0)
+
+# TEST THIRTEEN   !!!!!!!!!!!!!!!
+class Vehicle
+  def initialize(color, type)
+    @color = color
+    @type = type
+  end
+  def honk
+    puts "Beep!"
+  end
+  def more_honk
+    puts "Honk!"
+  end
+end
+
+moto = Vehicle.new("red", "Monster")
+moto.more_honk
