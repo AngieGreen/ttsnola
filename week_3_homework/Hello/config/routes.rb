300a5b6974d3cb03a7992f4a5c662d0ce5419679
Angie Green <angie@greenorleans.com>
2015-02-18 17:12:12 -0600

    Add contact page

--- week_3_homework/Hello/config/routes.rb
+++ week_3_homework/Hello/config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  get 'help' => 'static_pages#help'
 
-  get 'static_pages/help'
+  get 'about' => 'static_pages#about'
 
-  get 'static_pages/about'
+  get 'contact' => 'static_pages#contact'
 
 #this sets the pages/home file as the home page (index)
-  root to: 'static_pages#home'
+  root 'static_pages#home'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
